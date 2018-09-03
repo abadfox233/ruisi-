79e329d2bfbcc91551755d430356b2355bba13d7
abadfox233 <32118362+abadfox233@users.noreply.github.com>
2018-09-03 08:26:45 +0800

    Update creat_cookie.py

--- creat_cookie.py
+++ creat_cookie.py
@@ -21,19 +21,7 @@ class TransCookie:
 
 
 if __name__ == "__main__":
-    cookie = '''
-    Q8qA_2132_saltkey=NVyW437m; Q8qA_2132_lastvisit=1535858847; 
-    Q8qA_2132_sid=FxIlN6; Q8qA_2132_sendmail=1; 
-    Q8qA_2132_seccode=10354.599c10d54fe06ebcf1; 
-    Q8qA_2132_ulastactivity=4ac2vBXVf7tH85DFAKky%2F3oefsWsJM6uI3vmLWx359YcD7xcRAaB; 
-    Q8qA_2132_lastcheckfeed=301262%7C1535862490; 
-    Q8qA_2132_checkfollow=1; 
-    Q8qA_2132_lip=10.170.19.186%2C1535861086; 
-    Q8qA_2132_myrepeat_rr=R0; 
-    Q8qA_2132_auth=7eb6w1y31iagwjKG%2Bo%2BlUSpMu%2FmH3K7txjdAU5R9tp5H2HdQen%2BxLlJlyk4o2Dc%2BGTd6uZAmN%2FKaXItxJ%2F%2B%2FFTzHwyQ; 
-    Q8qA_2132_checkpm=1; 
-    Q8qA_2132_lastact=1535862494%09misc.php%09patch
-    '''
+    cookie = '''你的cookie'''
     trans = TransCookie(cookie)
     with open("1.txt", "wb") as f:
         pprint.pprint(trans.string_to_dict())
